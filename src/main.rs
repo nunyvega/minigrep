17953bcaabaeda823918cc3f54ec01b08b319da3
nunyvega <nunyvega@hotmail.com>
2023-10-23 18:18:58 +0200

    Add run func, add error for missing files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // eprintln! macro prints to stderr
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        // eprintln! macro prints to stderr
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?; // ? operator returns the error value from the current function for the caller to handle
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
@@ -21,14 +37,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
